c06ead2d858a8c0f0a50fc9f355f4f1b3b5d4697
LiuQhahah <1192395260@qq.com>
2018-06-08 10:30:02 +0800

    删除评论

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     #验证，title变量的长度最小为5
    validates :title, presence:true,length: {minimum:5}
 end
